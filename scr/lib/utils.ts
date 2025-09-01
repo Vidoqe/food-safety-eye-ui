ddfffad45f700b308cbb5d8e7e7fc177988a8841
Vidoqe <beautyfullpromos@gmail.com>
2025-09-01 11:48:25 +0800

    Update utils.ts

--- scr/lib/utils.ts
+++ scr/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
+// scr/utils/utils.ts
+
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Utility: Merge Tailwind + conditional classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
