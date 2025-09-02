4af96c968fe575b79957950d1c84b13799fa8d5c
Vidoqe <beautyfullpromos@gmail.com>
2025-09-02 08:54:48 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 // vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/scr", // 👈 point alias to scr (not src)
-    },
-  },
-  build: {
-    target: "esnext",
-    sourcemap: false,
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-  },
+  plugins: [react()]
 });
