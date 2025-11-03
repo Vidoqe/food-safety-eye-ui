e7a76f948625e90b516e5bb064f7659e92ecfd06
Vidoqe <beautyfullpromos@gmail.com>
2025-11-03 10:34:07 +0800

    restore App root with providers

--- scr/App.tsx
+++ scr/App.tsx
@@ -1,17 +1,46 @@
 
-import React, { Suspense, useState } from "react";
-import ErrorBoundary from "./components/ErrorBoundary";
-import LandingScreen from "./components/LandingScreen";
-
-// lazy-load the analyser screen (your working one)
-const FoodSafetyAnalyser = React.lazy(
-  () => import("./components/FoodSafetyAnalyser")
-);
-
 import React from "react";
+
+// providers (the wrappers that make language / user / history work)
+import { AppContextProvider } from "./contexts/AppContext";
+import { UserContextProvider } from "./contexts/UserContext";
+import { ScanHistoryProvider } from "./hooks/useScanHistory";
+
+// app layout + screens
+import AppLayout from "./components/AppLayout";
 import HomeScreen from "./components/HomeScreen";
 
 export default function App() {
-  console.log("[App] mounted ✅");
-  return <HomeScreen />;
+  console.log("[App] mounted ✅ (RESTORED)");
+
+  return (
+    <AppContextProvider>
+      <UserContextProvider>
+        <ScanHistoryProvider>
+          <AppLayout>
+            <HomeScreen
+              onScanLabel={() => {
+                console.log("TODO: go to Scan Label screen");
+              }}
+              onScanBarcode={() => {
+                console.log("TODO: go to Scan Barcode screen");
+              }}
+              onManualInput={() => {
+                console.log("TODO: go to Manual Input screen");
+              }}
+              onSettings={() => {
+                console.log("TODO: go to Settings screen");
+              }}
+              onScanHistory={() => {
+                console.log("TODO: go to Scan History screen");
+              }}
+              onApiTest={() => {
+                console.log("TODO: go to API Test screen");
+              }}
+            />
+          </AppLayout>
+        </ScanHistoryProvider>
+      </UserContextProvider>
+    </AppContextProvider>
+  );
 }
