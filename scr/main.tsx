ca072abd4c3da93f1970ed09e04e7c91fab052d9
Vidoqe <beautyfullpromos@gmail.com>
2025-11-05 10:25:40 +0800

    ensure tailwind css is loaded in main

--- scr/main.tsx
+++ scr/main.tsx
@@ -1,8 +1,13 @@
+// scr/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// ✅ this pulls in Tailwind classes
+import "./styles/tailwind.css";
+
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
