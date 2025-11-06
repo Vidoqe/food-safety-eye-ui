c098a732a6c4504842fd982f56bbe2d8674f9880
Vidoqe <beautyfullpromos@gmail.com>
2025-11-06 10:57:47 +0800

    fix: import Settings icon in HomeScreen

--- scr/components/HomeScreen.tsx
+++ scr/components/HomeScreen.tsx
@@ -1,6 +1,6 @@
 // scr/components/HomeScreen.tsx
 import React from "react";
-import { Shield, Heart, Camera, Scan, Type, Code } from "lucide-react";
+import { Shield, Heart, Camera, Scan, Type, Code, Settings } from "lucide-react";
 
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
