cd8259e94acf2e3133059c0dc5c6db483a9d1062
Vidoqe <beautyfullpromos@gmail.com>
2025-12-14 11:20:00 +0800

    Fix entry: render HomeScreen under providers

--- scr/pages/Index.tsx
+++ scr/pages/Index.tsx
@@ -1,34 +1,5 @@
-import React, { useState } from "react";
-import { AppProvider } from "@/contexts/AppContext";
-import { UserProvider } from "@/contexts/UserContext";
 import HomeScreen from "@/components/HomeScreen";
 
-const Index: React.FC = () => {
-  const [ingredients, setIngredients] = useState<any[]>([]);
-
-  const handleScan = async (payload: any) => {
-    try {
-      const response = await fetch(import.meta.env.VITE_SUPABASE_EDGE_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(payload),
-      });
-
-      const data = await response.json();
-      console.log("Scan result:", data);
-      setIngredients(data.ingredients || []);
-    } catch (err) {
-      console.error("Scan failed:", err);
-    }
-  };
-
-  return (
-    <UserProvider>
-      <AppProvider>
-        <HomeScreen onScan={handleScan} additives={ingredients} />
-      </AppProvider>
-    </UserProvider>
-  );
-};
-
-export default Index;
+export default function Index() {
+  return <HomeScreen />;
+}
