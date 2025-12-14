000a3fb9278ef6974916d43739a45de4544558d4
Vidoqe <beautyfullpromos@gmail.com>
2025-12-14 10:41:52 +0800

    Render Index from App

--- scr/App.tsx
+++ scr/App.tsx
@@ -1,34 +1,6 @@
 
-// scr/App.tsx
-import React from "react";
-import { Toaster } from "sonner";
-import * as Tooltip from "@radix-ui/react-tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "@/components/theme-provider";
-import { UserProvider } from "@/contexts/UserContext";
-
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
 
 export default function App() {
-  return (
-    <ThemeProvider defaultTheme="light">
-      <QueryClientProvider client={queryClient}>
-        <Tooltip.Provider>
-          <UserProvider>
-            <Toaster />
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
-          </UserProvider>
-        </Tooltip.Provider>
-      </QueryClientProvider>
-    </ThemeProvider>
-  );
+  return <Index />;
 }
--- scr/pages/Index.tsx
+++ scr/pages/Index.tsx
@@ -1,31 +1,11 @@
-// scr/pages/Index.tsx
-import React, { useState } from "react";
+import React from "react";
 import { AppProvider } from "@/contexts/AppContext";
 import HomeScreen from "@/components/HomeScreen";
 
 const Index: React.FC = () => {
-  const [ingredients, setIngredients] = useState<any[]>([]);
-
-  const handleScan = async (payload: any) => {
-    try {
-      const response = await fetch(import.meta.env.VITE_SUPABASE_EDGE_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(payload),
-      });
-
-      const data = await response.json();
-      console.log("Scan result:", data);
-
-      setIngredients(data.ingredients || []);
-    } catch (err) {
-      console.error("Scan failed:", err);
-    }
-  };
-
   return (
     <AppProvider>
-      <HomeScreen onScan={handleScan} additives={ingredients} />
+      <HomeScreen />
     </AppProvider>
   );
 };
