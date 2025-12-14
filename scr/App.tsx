919ee46df67645ae9ef806b5f6fd0b006c7a64b3
Vidoqe <beautyfullpromos@gmail.com>
2025-12-14 11:12:13 +0800

    Force render new HomeScreen

--- scr/App.tsx
+++ scr/App.tsx
@@ -1,13 +1,20 @@
 
-import { AppProvider } from "@/contexts/AppContext";
-import { UserProvider } from "@/contexts/UserContext";
-import Index from "@/pages/Index";
+import { AppProvider } from "./contexts/AppContext";
+import { UserProvider } from "./contexts/UserContext";
+import HomeScreen from "./components/HomeScreen";
 
 export default function App() {
   return (
     <UserProvider>
       <AppProvider>
-        <Index />
+        <HomeScreen
+          onScanLabel={() => console.log("Scan label")}
+          onScanBarcode={() => console.log("Scan barcode")}
+          onManualInput={() => console.log("Manual input")}
+          onSettings={() => console.log("Settings")}
+          onScanHistory={() => console.log("Scan history")}
+          onApiTest={() => console.log("API test")}
+        />
       </AppProvider>
     </UserProvider>
   );
