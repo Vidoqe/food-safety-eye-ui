4b2733f31fe80511c09a576025cfaa8fd2732947
Vidoqe <beautyfullpromos@gmail.com>
2026-08-21 11:38:02 +0800

    Fix manual ingredient input

--- scr/services/gptImageAnalysis.ts
+++ scr/services/gptImageAnalysis.ts
@@ -38,6 +38,7 @@ export default class GPTImageAnalysisService {
           apikey: anonKey,
         },
       body: JSON.stringify({
+        ingrdients: ingredients ?? "",
   imageBase64:
   typeof imageBase64 === "string"
     ? imageBase64
